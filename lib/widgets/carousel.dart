7c039631073d79d190cb817313a600377719131f
yasaleo <yadhusanil26@gmail.com>
2023-05-21 13:07:17 +0530

    carosel slider inititation

---
+++ lib/widgets/carousel.dart
@@ -0,0 +1,36 @@
+import 'package:flutter/material.dart';
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({
+    super.key,
+  });
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+        title: const Text(
+          "Carousel",
+          style: TextStyle(fontWeight: FontWeight.bold),
+        ),
+      ),
+      body: const Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Text(
+              '',
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
