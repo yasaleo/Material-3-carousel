682846d887a3f91cdb40b416fff75d80e5df5f8c
Mohana Vaishali <mohanavaishali.k@bluescopetech.com>
2023-05-31 16:18:08 +0530

    new repo

--- lib/widgets/carousel.dart
+++ lib/widgets/carousel.dart
@@ -26,7 +26,7 @@ class _MyHomePageState extends State<MyHomePage> {
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
             Text(
-              '',
+              'Hai',
             ),
           ],
         ),
